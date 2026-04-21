46ca10f88e409534b386dcc54a041a59eb5e4cc1
Jason31224 <jason180105@icloud.com>
2026-04-21 17:56:44 +0200

    .

---
+++ src/frontend/Launcher.java
@@ -0,0 +1,6 @@
+public class Launcher {
+    public static void main(String[] args) {
+        // Dieser Aufruf trickst die strengen JavaFX-Sicherheitsregeln aus
+        EasyRideApp.main(args);
+    }
+}
