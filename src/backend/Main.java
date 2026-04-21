c0e65db022e804f9f01101b49de84cfa7ac8ddcb
Jason31224 <jason180105@icloud.com>
2026-04-21 21:46:50 +0200

    update blya

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -2,7 +2,7 @@ import javafx.application.Application;
 import ui.EasyRideApp;
 
 public class Main {
-    // Die eckigen Klammern [] sind Pflicht, sonst fehlt der Play-Button!
+
     public static void main(String[] args) {
         EasyRideApp.main(args);
     }
